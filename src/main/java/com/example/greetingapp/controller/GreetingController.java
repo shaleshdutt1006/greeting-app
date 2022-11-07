66b883a0105682327dd49688dc455bc5cacf2868
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-11-07 21:46:45 +0530

    Added: Using GreetingController return various http method

---
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -0,0 +1,27 @@
+package com.example.greetingapp.controller;
+
+import com.example.greetingapp.model.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    // An AtomicLong is used in applications such as automatically incremented sequence number
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    // name has defaultValue world we can change it using Postman
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        // return a new Greeting class object with id values incremented by 1 every time and String
+        //format to show message of hello and the name we pass on the Postman
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+
+
+}
---
+++ src/main/java/com/example/greetingapp/repository/GreetingAppRepository.java
@@ -0,0 +1,8 @@
+package com.example.greetingapp.repository;
+
+import com.example.greetingapp.model.Greeting;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GreetingAppRepository extends JpaRepository<Greeting, Integer> {
+
+}
