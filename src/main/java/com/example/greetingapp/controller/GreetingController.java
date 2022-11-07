344213a65fd35f1c78b26123da8041931444ba94
shaleshdutt1006 <shaleshdutt1006@gmail.com>
2022-11-07 21:47:37 +0530

    Added: Using GreetingController return various http method

--- src/main/java/com/example/greetingapp/controller/GreetingController.java
+++ src/main/java/com/example/greetingapp/controller/GreetingController.java
@@ -16,7 +16,7 @@ public class GreetingController {
     private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
-    // name has defaultValue world we can change it using Postman
+    // name has defaultValue world we change it using Postman
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         // return a new Greeting class object with id values incremented by 1 every time and String
         //format to show message of hello and the name we pass on the Postman
